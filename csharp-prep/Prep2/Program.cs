fe2a5979bfa94e2c2088cd58f451301c0929e731
Michael Scoville <sco20014@byui.edu>
2024-04-23 11:57:16 -0600

    I completed Prep2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage grade did you earn? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        if (grade >=90)
+        {
+            Console.WriteLine("You have earned an A!");
+        }
+        else if (grade >=80)
+        {
+            Console.WriteLine("You have earned a B!");
+        }
+        else if (grade >=70)
+        {
+            Console.WriteLine("You have earned a C.");
+        }
+        else if (grade >=60)
+        {
+            Console.WriteLine("You have earned a D.");
+        }
+        else 
+        {
+            Console.WriteLine("You have earned an F.");
+        }
+
+        if (grade >= 70)
+        {
+            Console.Write("Great job! You have passed the class.");
+        }
+        else 
+        {
+            Console.Write("I'm so sorry! You haven't passed this class.");
+        }
+
+
     }
 }

48390455a502aaac2f9a4d1a53e686ac3129b1d1
Michael Scoville <sco20014@byui.edu>
2024-05-02 10:50:40 -0600

    I completed Prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+   {
+        Random randomGenerator = new Random();
+        int magicNumberMS = randomGenerator.Next(1, 101);
+
+        int guessMS = -1;
+
+        while (guessMS != magicNumberMS)
+        {
+            Console.Write("What is your guess? ");
+            guessMS = int.Parse(Console.ReadLine());
+
+            if (magicNumberMS > guessMS)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumberMS < guessMS)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }

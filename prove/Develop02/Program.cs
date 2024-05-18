5d1e88911940b09ef3086249cef5976029222d42
Michael Scoville <sco20014@byui.edu>
2024-05-18 10:24:25 -0600

    Added a comment that explained what I added to exceed the requirements of the program.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,3 +1,9 @@
+//This is my Journal Program! To exceed the requirements of the assignment, I decided to add a new menu option to the program: Motivate me!
+//I used my Prompt Generator class to also store a list of motivational tid-bits that I wrote. 
+//When the user selects this option, it randomly selects one of these quotes and prints it to the screen.
+//The idea behind this is that it will give the user a little bit of motivation regarded to actually keeping a journal in the first place.
+//This is a thing I personally struggle with, so this feature was meant for people like me, who struggle to start a consistent journal.
+
 using System;
 using System.Runtime.InteropServices;
 

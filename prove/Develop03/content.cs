bd6b2c5cd0b752f58c2b5929aa4f70250c9be49c
Michael Scoville <sco20014@byui.edu>
2024-05-29 08:08:07 -0600

    Created variables for each class

---
+++ prove/Develop03/content.cs
@@ -0,0 +1,8 @@
+class Content
+{
+    private List<Word> _words;
+
+    private Reference _reference;
+
+    private Random _random;
+}
---
+++ prove/Develop03/word.cs
@@ -0,0 +1,7 @@
+class Word
+{
+    private string _word;
+
+    private bool _isHidden;
+}
+

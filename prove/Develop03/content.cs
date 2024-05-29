8edb24125904bd339edc1342bbe8f087dee71598
Michael Scoville <sco20014@byui.edu>
2024-05-29 08:35:05 -0600

    The constructor for my Content is completed

--- prove/Develop03/content.cs
+++ prove/Develop03/content.cs
@@ -5,4 +5,17 @@ class Content
     private Reference _reference;
 
     private Random _random;
+
+    public Content(Reference reference, string text)
+    {
+        _reference = reference;
+        _random = new();
+        _words = [];
+
+        foreach (string word in text.Split(" "))
+        {
+            _words.Add(new Word(word));
+        }
+    }
 }
+

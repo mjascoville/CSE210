d52fabf46c697b233224868a87b0e46641485a74
Michael Scoville <sco20014@byui.edu>
2024-06-06 09:30:11 -0600

    I finished Learning ACtivity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Michael Scoville", "E=MC^2");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Tom Thumb", "Decimals", "7.3", "8 and 9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Frederick Douglas", "American History", "The Grapes of Wrath");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }

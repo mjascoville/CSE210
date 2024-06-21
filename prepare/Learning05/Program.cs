575e09592d2c169e11132aae4f890de4ab751e71
Michael Scoville <sco20014@byui.edu>
2024-06-21 12:06:38 -0600

    Completed Learning Activity 5.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Purple", 1);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 2, 3);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Teal", 5);
+        shapes.Add(s3);
+
+        Console.WriteLine("Curious about these shapes?");
+        Console.WriteLine("");
+
+        foreach (Shape s in shapes)
+        {
+            double area = s.GetArea();
+
+            string color = s.GetColor();
+
+            Console.WriteLine($"The {color} shape has an area of {area}!");
+        }
     }
 }
